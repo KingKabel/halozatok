0d4414f4b31aae35bc7a62db5f6b082fc4d7a78b
KingKabel <abel.kovats@stud.uni-corvinus.hu>
2021-03-18 13:16:16 +0100

    szorzótábla és Pascal-piramis javascript oldalak hozzáadva

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,23 @@
+﻿window.onload = () => {
+    console.log("pascal start")
+    var hova = document.getElementById("pascal");
+    for (var sor = 0; sor < 10; sor++) {
+        var aktuálisSor = document.createElement("div");
+        aktuálisSor.classList.add("sor")
+        hova.appendChild(aktuálisSor)
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var aktuálisElem = document.createElement("div");
+            aktuálisElem.classList.add("elem")
+            let e = faktoriális(sor) / (faktoriális(oszlop) * faktoriális(sor - oszlop))
+            aktuálisElem.innerHTML = e
+            aktuálisSor.appendChild(aktuálisElem)
+        }
+    }
+}
+var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
---
+++ HajosTeszt/HajosTeszt/wwwroot/szorzo.js
@@ -0,0 +1,17 @@
+﻿window.onload = function () {
+    console.log("start")
+    let hova = document.getElementById("ide");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor")
+        hova.appendChild(sor);
+
+        for (var o = 0; o < 10; o++) {
+            let szám = document.createElement("div");
+            szám.classList.add("elem")
+            szám.innerText = (s + 1) * (o + 1)
+            sor.appendChild(szám);
+            szám.style.background = `rgb(${255 / 10 * o},0,0)`
+        }
+    }
+}
