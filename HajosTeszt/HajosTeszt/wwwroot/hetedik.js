187ddaf344d49a5be2b3b8e94ac12a40f559adfa
KingKabel <abel.kovats@stud.uni-corvinus.hu>
2021-03-25 17:55:12 +0100

    első háromkérdés megjelenítve, továbbkattintható

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,73 @@
+﻿var kérdésCount = 0;
+var kérdések;
+window.onload = () => {
+    console.log("letöltés start");
+    letöltés();
+}
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés");
+    console.log(d);
+    console.log(`${d.length} kérdés érkezett`);
+    kérdések = d;
+    kérdésMegjelenítés(kérdésCount);
+}
+function letöltés () {
+        fetch('/questions.json')
+            .then(response => response.json())
+            .then(data => letöltésBefejeződött(data)
+            );
+}
+function kérdésMegjelenítés(k) {
+    document.getElementById("kérdés_szöveg").innerHTML = kérdések[k].questionText;
+
+    document.getElementById("válasz1").innerHTML = kérdések[k].answer1;
+
+    if (kérdések[k].correctAnswer == 1) {
+        document.getElementById("válasz1").className = "kerdes kattintható jó";
+    } else {
+        document.getElementById("válasz1").className = "kerdes kattintható rossz";
+    }
+
+
+    document.getElementById("válasz2").innerHTML = kérdések[k].answer2;
+
+    if (kérdések[k].correctAnswer == 2) {
+        document.getElementById("válasz2").className = "kerdes kattintható jó";
+    } else {
+        document.getElementById("válasz2").className = "kerdes kattintható rossz";
+    }
+
+
+    document.getElementById("válasz3").innerHTML = kérdések[k].answer3;
+
+    if (kérdések[k].correctAnswer == 3) {
+        document.getElementById("válasz3").className = "kerdes kattintható jó";
+    } else {
+        document.getElementById("válasz3").className = "kerdes kattintható rossz";
+    }
+
+
+    if (kérdések[k].image == "") {
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[k].image;
+        document.getElementById("kép1").alt = "Nincsen megjeleníthető kép";
+    } else {
+        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[k].image;
+    }
+}
+
+function előre() {
+    if (kérdésCount == 2) {
+        kérdésCount = 0;
+    } else {
+        kérdésCount++;
+    }
+    kérdésMegjelenítés(kérdésCount);
+}
+function vissza() {
+    if (kérdésCount == 0) {
+        kérdésCount = 2;
+    } else {
+        kérdésCount--;
+    }
+    kérdésMegjelenítés(kérdésCount);
+}
